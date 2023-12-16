c04685c04da232294b42134375fc21fbfc03c9e2
devlarisarapio <larissa.sarapio@gmail.com>
2023-12-16 17:58:44 -0300

    insere a nova versão do projeto intergrado com mondodb

--- mulheres.js
+++ mulheres.js
@@ -1,34 +1,101 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express") // aqui estou iniciando o express
+const router = express.Router() // aqui estou configurando a primeira parte da rota
+const cors = require('cors') // aqui estou trazendo o pacote cors que permite consumir essa API no frontend
+const conectaBancoDeDados = require('./bancoDeDados') //ligando ao arquivo bancoDeDados
+conectaBancoDeDados() // estou chamando a função que conecta o banco de dados
 
-const app = express ()
-const porta = 3333
+const Mulher = require('./mulherModel')
 
-const mulheres = [
-    {
-        nome: 'Larissa Sarapio',
-        imagem: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fveja.abril.com.br%2Fcoluna%2Fveja-gente%2Fo-acordo-de-larissa-manoela-com-os-pais-apos-batalha-na-justica&psig=AOvVaw0efL4SQ3Td-Ro1yTR3IYsY&ust=1702205123200000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNDrkrCWgoMDFQAAAAAdAAAAABAD',
-        minibio: 'Desenvolvedora backend - java e python'
-    },
-    {
-        nome: 'Iana Chan',
-        imagem: 'https://bit.ly/3JCWBqP',
-        miniobio: 'Programadora da PrograMaria'
-    },
-    {
-        nome: 'Nina da hora',
-        imagem: 'https://bit.ly/3FKpFaz',
-        miniobio: 'Hacker antirracista'
+const app = express () // aqui estou iniciando o app
+app.use(express.json())
+app.use(cors())
+
+const porta = 3333 // criando a porta
+
+//GET - mostra mulheres
+async function mostraMulheres(request, response) {
+    try{
+        const mulheresVindasDoBancoDeDados = await Mulher.find()
+
+        response.json(mulheresVindasDoBancoDeDados)
+    }catch (erro) {
+        console.log(erro)
     }
-]
-
-function mostraMulheres(request, response) {
-    response.json(mulheres)
 }
 
+//POST - cria novas mulheres
+async function criarMulher(request, response) {
+    const novaMulher = new Mulher( {
+        nome: request.body.nome,
+        imagem: request.body.imagem,
+        minibio: request.body.minibio,
+        citacao: request.body.citacao
+
+    })
+
+    try {
+        const mulherCriada = await novaMulher.save()
+        response.status(201).json(mulherCriada)
+    } catch (erro) {
+        console.log()
+    }
+
+
+}
+
+//PATCH
+async function corrigeMulher(request, response) {
+    try {
+        const mulherEncontrada = await Mulher.findById(request.params.id)
+
+        if (request.body.nome) {
+            mulherEncontrada.nome = request.body.nome
+        }
+    
+        if (request.body.minibio) {
+            mulherEncontrada.minibio = request.body.minibio
+        }
+    
+        if (request.body.imagem) {
+            mulherEncontrada.imagem = request.body.imagem
+        }
+
+        if (request.body.citacao) {
+            mulherEncontrada.citacao = request.body.citacao
+        }
+
+        const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save()
+        
+        response.json(mulherAtualizadaNoBancoDeDados)
+
+
+    } catch (erro) {
+        console.log(erro)
+    } 
+
+}
+
+//DELETE
+async function deletaMulher(request, response){
+    try {
+        await Mulher.findByIdAndDelete(request.params.id)
+        response.json({ mensagem: 'Mulher deletada com sucesso!'})
+    } catch (erro){
+        console.log(erro)
+    }
+
+}
+
+app.use(router.get('/mulheres', mostraMulheres)) // configurei a rota GET/mulheres
+app.use(router.post('/mulheres',criarMulher)) // configurei rota POST/mulheres
+app.use(router.patch('/mulheres/:id', corrigeMulher)) // configurei a rota PATCH/mulheres/id
+app.use(router.delete('/mulheres/:id',deletaMulher)) //configurei a rota DELETE/mulheres
+
+
+//PORTA
 function mostraPorta() {
     console.log("Servidor criado e rodando na porta ", porta)
 }
 
-app.use(router.get('/mulheres'), mostraMulheres)
-app.listen(porta, mostraPorta)
+
+app.listen(porta, mostraPorta) //servidor ouvindo a porta 
